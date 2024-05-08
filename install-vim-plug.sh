aa54f7b0047532c6df2ddc361ffb6443277e468f
Dustin Seger <dustin.seger@hotmail.com>
2024-05-07 20:13:14 -0400

    Fix Linux vim-plug install script pathing

--- install-vim-plug.sh
+++ install-vim-plug.sh
@@ -1,1 +1,1 @@
-sh -c 'curl -fLo /home/lucifer/.local/share/nvim/site/autoload/plug.vim --create-dirs        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+sh -c 'curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
